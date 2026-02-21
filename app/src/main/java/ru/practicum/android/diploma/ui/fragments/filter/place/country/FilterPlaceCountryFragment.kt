2be73dc121d48381afc76f2528690cdd1738217e
nkyancen <nkyancen@yandex.ru>
2026-02-21 21:15:51 +0300

    Добавлены заглушки экранов и граф навигации

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/filter/place/country/FilterPlaceCountryFragment.kt
@@ -0,0 +1,26 @@
+package ru.practicum.android.diploma.ui.fragments.filter.place.country
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FragmentFilterPlaceCountryBinding
+
+class FilterPlaceCountryFragment : Fragment() {
+    private var _binding: FragmentFilterPlaceCountryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFilterPlaceCountryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/filter/place/region/FilterPlaceRegionFragment.kt
@@ -0,0 +1,26 @@
+package ru.practicum.android.diploma.ui.fragments.filter.place.region
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FragmentFilterPlaceRegionBinding
+
+class FilterPlaceRegionFragment : Fragment() {
+    private var _binding: FragmentFilterPlaceRegionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFilterPlaceRegionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
