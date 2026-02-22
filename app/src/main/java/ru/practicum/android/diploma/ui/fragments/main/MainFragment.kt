c6548b3ef278d9d3f4a91288e932b78954425b16
AlpRem <test@me.comm>
2026-02-22 23:00:06 +0300

    add fragment job search

--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/main/MainFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/main/MainFragment.kt
@@ -1,13 +1,19 @@
 package ru.practicum.android.diploma.ui.fragments.main
 
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentMainBinding
+import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
@@ -29,16 +35,33 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.mainVacancyButton.setOnClickListener {
-            findNavController().navigate(
-                R.id.action_mainFragment_to_vacancyDetailsFragment
-            )
-        }
-
-        binding.mainFilterButton.setOnClickListener {
+        onInitListener()
+//        binding.mainVacancyButton.setOnClickListener {
+//            findNavController().navigate(
+//                R.id.action_mainFragment_to_vacancyDetailsFragment
+//            )
+//        }
+//
+        binding.filter.setOnClickListener {
             findNavController().navigate(
                 R.id.action_mainFragment_to_filterFragment
             )
         }
+    }
+
+    private fun onInitListener() {
+        binding.editTextboxJobSearch.doOnTextChanged { text, _, _, _ ->
+            updateIcons(!text.isNullOrEmpty())
+        }
+        binding.iconClear.setOnClickListener {
+            binding.editTextboxJobSearch.text?.clear()
+            val ims = requireContext().getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+            ims?.hideSoftInputFromWindow(binding.editTextboxJobSearch.windowToken, 0)
+        }
+    }
+
+    private fun updateIcons(hasText: Boolean) = with(binding) {
+        binding.iconClear.isVisible = hasText
+        binding.iconSearch.isVisible = !hasText
     }
 }
