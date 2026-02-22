e06e0943193cfcc43f8418e2c8cc6660491f36ed
nkyancen <nkyancen@yandex.ru>
2026-02-22 10:16:36 +0300

    настроено BottomNavMenu

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -2,20 +2,54 @@ package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.data.network.NetworkResult
 import ru.practicum.android.diploma.data.repository.YPApiRepository
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRootBinding
     private val repository: YPApiRepository by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val paddingBottom = if (binding.rootBottomNavMenu.isVisible) {
+                0
+            } else {
+                systemBars.bottom
+            }
+            v.setPadding(systemBars.left, systemBars.top, systemBars.left, paddingBottom)
+            insets
+        }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.root_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.rootBottomNavMenu.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.rootBottomNavMenu.isVisible =
+                (destination.id == R.id.main_fragment ||
+                    destination.id == R.id.favorites_fragment ||
+                    destination.id == R.id.team_fragment)
+        }
 
         // Пример запроса к API
         networkRequestExample()
