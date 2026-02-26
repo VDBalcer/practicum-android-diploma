745ecbdee34e40d35cacfa46a94e73af0b17e3d9
nkyancen <nkyancen@yandex.ru>
2026-02-26 19:35:29 +0300

    привел мапперы к extension-функциям fix after detekt

--- app/src/main/java/ru/practicum/android/diploma/presentation/mapper/FilteredIndustryMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/mapper/FilteredIndustryMapper.kt
@@ -4,13 +4,13 @@ import ru.practicum.android.diploma.domain.models.FilterIndustryModel
 import ru.practicum.android.diploma.presentation.model.FilteredIndustryItem
 
 fun FilteredIndustryItem.toDomain(): FilterIndustryModel =
-        FilterIndustryModel(
-            id = id,
-            name = name
-        )
+    FilterIndustryModel(
+        id = id,
+        name = name
+    )
 
 fun FilterIndustryModel.toItem(): FilteredIndustryItem =
-        FilteredIndustryItem(
-            id = id,
-            name = name
-        )
+    FilteredIndustryItem(
+        id = id,
+        name = name
+    )
