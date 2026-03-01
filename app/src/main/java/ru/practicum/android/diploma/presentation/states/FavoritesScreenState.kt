6dd430383fd84e732576d4c8ae8ee64e29aedbab
nkyancen <nkyancen@yandex.ru>
2026-03-01 19:44:52 +0300

    Отображение списка избранных вакансий fix after detekt

--- app/src/main/java/ru/practicum/android/diploma/presentation/states/FavoritesScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/states/FavoritesScreenState.kt
@@ -3,9 +3,9 @@ package ru.practicum.android.diploma.presentation.states
 import ru.practicum.android.diploma.presentation.model.VacancyItem
 
 sealed class FavoritesScreenState {
-    object Loading: FavoritesScreenState()
-    object EmptyFavorites: FavoritesScreenState()
-    object DBError: FavoritesScreenState()
+    object Loading : FavoritesScreenState()
+    object EmptyFavorites : FavoritesScreenState()
+    object DBError : FavoritesScreenState()
 
     data class Content(
         val content: List<VacancyItem>
